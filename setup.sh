86d1102240ba46bd5794e1f8d4ba08195971a73b
Mireu / 미르 <97509351+Mireu-Lab@users.noreply.github.com>
2022-04-18 02:26:09 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,16 +5,16 @@ sudo apt-get -y upgrade
 sudo apt-get -y autoremove
 
 sudo apt-get -y install unzip nano htop wget net-tools ufw mc git
-sudo apt-get -y install gnome-tweak-tool fonts-nanum fonts-nanum-extra fonts-nanum-coding fcitx-hangul
+sudo apt-get -y install gnome-tweak-tool
 
 echo "우분투 테마 설치을 진행합니다.\n"
 #테마 : https://github.com/vinceliuice/Orchis-theme
 git clone https://github.com/vinceliuice/Orchis-theme.git
-cd Orchis-theme && ./install.sh && cd ..
+cd Orchis-theme && ./install.sh -y && cd ..
 
 #아이콘 : https://github.com/vinceliuice/Colloid-icon-theme
 git clone https://github.com/vinceliuice/Colloid-icon-theme.git
-cd Colloid-icon-theme && ./install.sh && cd ..
+cd Colloid-icon-theme && ./install.sh -y && cd ..
 
 rm -rf Colloid-icon-theme Orchis-theme
 echo "우분투 기본 설치을 진행완료하였습니다.\n"
