351a755e4dd499d4f378aaed9fb81a447fc865be
Mireu / 미르 <97509351+Mireu-Lab@users.noreply.github.com>
2022-04-18 02:25:23 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,11 +10,11 @@ sudo apt-get -y install gnome-tweak-tool fonts-nanum fonts-nanum-extra fonts-nan
 echo "우분투 테마 설치을 진행합니다.\n"
 #테마 : https://github.com/vinceliuice/Orchis-theme
 git clone https://github.com/vinceliuice/Orchis-theme.git
-cd /Orchis-theme && ./install.sh && cd ..
+cd Orchis-theme && ./install.sh && cd ..
 
 #아이콘 : https://github.com/vinceliuice/Colloid-icon-theme
 git clone https://github.com/vinceliuice/Colloid-icon-theme.git
-cd /Orchis-theme && ./install.sh && cd ..
+cd Colloid-icon-theme && ./install.sh && cd ..
 
 rm -rf Colloid-icon-theme Orchis-theme
 echo "우분투 기본 설치을 진행완료하였습니다.\n"
